f9d3f6848632b1068e82d5219947a83ba81f40b6
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-01-24 18:21:46 +0100

    Auto documention

--- micrometer-tracing/src/main/java/io/micrometer/tracing/docs/DocumentedSpan.java
+++ micrometer-tracing/src/main/java/io/micrometer/tracing/docs/DocumentedSpan.java
@@ -16,20 +16,27 @@
 
 package io.micrometer.tracing.docs;
 
+import io.micrometer.api.instrument.docs.DocumentedSample;
+import io.micrometer.api.instrument.docs.TagKey;
+import io.micrometer.tracing.handler.TracingRecordingHandler;
+
 /**
  * In order to describe your spans via e.g. enums instead of Strings you can use this
- * interface that returns all the characteristics of a span. In Spring Cloud Sleuth we
+ * interface that returns all the characteristics of a span. In Micrometer Tracing we
  * analyze the sources and reuse this information to build a table of known spans, their
  * names, tags and events.
  *
  * We can generate documentation for all created spans but certain requirements need to be
  * met
  *
- * - spans are grouped within an enum - the enum implements the {@link DocumentedSpan}
- * interface - if the span contains {@link TagKey} or {@link EventValue} then those need
- * to be declared as nested enums - the {@link DocumentedSpan#getTagKeys()} and
- * {@link DocumentedSpan#getEvents()} need to call the nested enum's {@code values()}
- * method to retrieve the array of allowed keys / events
+ * <ul>
+ *     <li>metrics are grouped within an enum - the enum implements the {@link DocumentedSpan} interface</li>
+ *     <li>if the span contains {@link TagKey} then those need to be declared as nested enums</li>
+ *     <li>if the span contains {@link EventValue} then those need to be declared as nested enums</li>
+ *     <li>the {@link DocumentedSpan#getTagKeys()} need to call the nested enum's {@code values()} method to retrieve the array of allowed keys / events</li>
+ *     <li>the {@link DocumentedSpan#getEvents()} ()} need to call the nested enum's {@code values()} method to retrieve the array of allowed keys / events</li>
+ *     <li>Javadocs around enums will be used as description</li>
+ * </ul>
  *
  * @author Marcin Grzejszczak
  * @since 1.0.0
@@ -42,10 +49,15 @@ public interface DocumentedSpan {
     String getName();
 
     /**
-     * @return allowed tag keys
+     * Builds a name from provided vars. Follows the {@link String#format(String, Object...)} patterns.
+     * @param vars variables to pass to {@link String#format(String, Object...)}
+     * @return constructed name
      */
-    default TagKey[] getTagKeys() {
-        return new TagKey[0];
+    default String getName(String... vars) {
+        if (getName().contains("%s")) {
+            return String.format(getName(), (Object[]) vars);
+        }
+        return getName();
     }
 
     /**
@@ -56,13 +68,27 @@ public interface DocumentedSpan {
     }
 
     /**
-     * Returns required prefix to be there for events and tags. Example {@code foo.} would
-     * require the tags and events to have a {code foo} prefix like this for tags:
-     * {@code foo.bar=true} and {@code foo.started} for events.
-     * @return required prefix
+     * @return allowed tag keys - if set will override any tag keys coming from {@link DocumentedSpan#overridesDefaultSpanFrom()}
      */
-    default String prefix() {
-        return "";
+    default TagKey[] getTagKeys() {
+        return new TagKey[0];
     }
 
+    /**
+     * @return additional tag keys - if set will append any tag keys coming from {@link DocumentedSpan#overridesDefaultSpanFrom()}
+     */
+    default TagKey[] getAdditionalTagKeys() {
+        return new TagKey[0];
+    }
+
+    /**
+     * Provide a {@link DocumentedSample} class whose default
+     * span creation should be ignored and will be overridden by this implementation. This should be overridden when
+     * you have a custom {@link TracingRecordingHandler} that sets up spans in a custom fashion.
+     *
+     * @return {@link DocumentedSample} class
+     */
+    default DocumentedSample overridesDefaultSpanFrom() {
+        return null;
+    }
 }
