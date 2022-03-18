280b7caa5b7696b79da72f38ff82a30a78d913d2
Moritz Halbritter <mhalbritter@vmware.com>
2022-03-18 11:07:47 +0100

    Implement no-op version of SpanMetrics

--- micrometer-tracing-reporters/micrometer-tracing-reporter-wavefront/src/main/java/io/micrometer/tracing/reporter/wavefront/SpanMetrics.java
+++ micrometer-tracing-reporters/micrometer-tracing-reporter-wavefront/src/main/java/io/micrometer/tracing/reporter/wavefront/SpanMetrics.java
@@ -17,18 +17,71 @@
 package io.micrometer.tracing.reporter.wavefront;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
+ * Reports metrics from {@link WavefrontSpanHandler}.
+ *
  * @author Moritz Halbritter
+ * @since 1.0.0
  */
 public interface SpanMetrics {
-    long reportDropped();
+	/**
+	 * Is called when a span has been dropped.
+	 *
+	 * @return number of total dropped spans
+	 */
+	long reportDropped();
 
-    void reportReceived();
+	/**
+	 * Is called when a span is received.
+	 */
+	void reportReceived();
 
-    void reportErrors();
+	/**
+	 * Is called when a span couldn't be sent.
+	 */
+	void reportErrors();
 
-    void registerQueueSize(BlockingQueue<?> queue);
+	/**
+	 * Registers the size of the given {@code queue}.
+	 *
+	 * @param queue queue which size should be registered
+	 */
+	void registerQueueSize(BlockingQueue<?> queue);
 
-    void registerQueueRemainingCapacity(BlockingQueue<?> queue);
+	/**
+	 * Registers the remaining capacity of the given {@code queue}.
+	 *
+	 * @param queue queue which remaining capacity should be registered
+	 */
+	void registerQueueRemainingCapacity(BlockingQueue<?> queue);
+
+	/**
+	 * No-op implementation.
+	 */
+	SpanMetrics NOOP = new SpanMetrics() {
+		private final AtomicLong dropped = new AtomicLong();
+
+		@Override
+		public long reportDropped() {
+			return dropped.incrementAndGet();
+		}
+
+		@Override
+		public void reportReceived() {
+		}
+
+		@Override
+		public void reportErrors() {
+		}
+
+		@Override
+		public void registerQueueSize(BlockingQueue<?> queue) {
+		}
+
+		@Override
+		public void registerQueueRemainingCapacity(BlockingQueue<?> queue) {
+		}
+	};
 }
