a5f08fa5f38164bd7ddda3f69b6941489ebb98e1
Marcin Grzejszczak <marcin@grzejszczak.pl>
2024-01-11 14:56:33 +0100

    Polish

--- micrometer-tracing-bridges/micrometer-tracing-bridge-brave/src/main/java/io/micrometer/tracing/brave/bridge/W3CPropagation.java
+++ micrometer-tracing-bridges/micrometer-tracing-bridge-brave/src/main/java/io/micrometer/tracing/brave/bridge/W3CPropagation.java
@@ -179,11 +179,6 @@ public class W3CPropagation extends Propagation.Factory implements Propagation<S
     }
 
     @Override
-    public Propagation<String> get() {
-        return this;
-    }
-
-    @Override
     public List<String> keys() {
         return FIELDS;
     }
