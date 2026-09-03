997f4046e281a20f8ea19a8526c749700277dabd
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2026-09-03 11:28:05 +0900

    Make aopalliance dependency optional (#1543)
    Prior to this change, micrometer-tracing declared a mandatory API
    dependency on aopalliance:aopalliance:1.0. This caused split-package
    resolution failures on the Java Module Path (JPMS) when spring-aop was
    also present, as well as non-deterministic classpath shadowing.
    
    This change:
    - Changes aopalliance to an optionalApi dependency in micrometer-tracing.
    - Excludes non-published internal test modules from Maven publishing and the
      BOM while preserving micrometer-tracing-integration-test.
    - Adds internal integration test modules under internal/ to verify:
      - Spring AOP + AspectJ proxying using spring-aop's bundled AOP Alliance
        interfaces without aopalliance:1.0.
      - Failures and limitations when attempting aspect proxying without
        aspectjweaver with Spring AOP.
      - JPMS module path resolution and split-package conflict detection.
    
    Closes gh-1536

---
+++ internal/micrometer-tracing-integration-test-jpms/src/test/java/io/micrometer/tracing/integration/jpms/JpmsModulePathIntegrationTests.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.tracing.integration.jpms;
+
+import io.micrometer.tracing.Tracer;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+import java.io.File;
+import java.lang.module.Configuration;
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.lang.module.ResolutionException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class JpmsModulePathIntegrationTests {
+
+    @Test
+    void shouldFindMicrometerTracingAsAutomaticModule() throws URISyntaxException {
+        Path tracingPath = getPathForClass(Tracer.class);
+        ModuleFinder finder = ModuleFinder.of(tracingPath);
+
+        Optional<ModuleReference> moduleRef = finder.find("micrometer.tracing");
+        assertThat(moduleRef).isPresent();
+
+        ModuleDescriptor descriptor = moduleRef.get().descriptor();
+        assertThat(descriptor.name()).isEqualTo("micrometer.tracing");
+        assertThat(descriptor.isAutomatic()).isTrue();
+    }
+
+    @Test
+    void shouldResolveTracingAndSpringAopOnModulePathWithoutSplitPackageError() throws URISyntaxException {
+        Path tracingPath = getPathForClass(Tracer.class);
+        Path springAopPath = getPathForClass(ProxyFactory.class);
+
+        ModuleFinder finder = ModuleFinder.of(tracingPath, springAopPath);
+
+        Configuration parentConfig = ModuleLayer.boot().configuration();
+        Configuration resolvedConfig = parentConfig.resolve(finder, ModuleFinder.of(),
+                Set.of("micrometer.tracing", "spring.aop"));
+
+        assertThat(resolvedConfig.findModule("micrometer.tracing")).isPresent();
+        assertThat(resolvedConfig.findModule("spring.aop")).isPresent();
+    }
+
+    @Test
+    void shouldFailWhenBothSpringAopAndAopAllianceAreOnModulePathDueToSplitPackage() throws URISyntaxException {
+        Path springAopPath = getPathForClass(ProxyFactory.class);
+        Path aopAlliancePath = getAopAlliancePath();
+
+        ModuleFinder finder = ModuleFinder.of(springAopPath, aopAlliancePath);
+
+        Configuration parentConfig = ModuleLayer.boot().configuration();
+
+        assertThatThrownBy(() -> parentConfig.resolve(finder, ModuleFinder.of(), Set.of("spring.aop", "aopalliance")))
+            .isInstanceOf(ResolutionException.class)
+            .hasMessageContaining("org.aopalliance");
+    }
+
+    @Test
+    void shouldFailWhenTracingResolvesBothSpringAopAndTransitiveAopAllianceOnModulePath() throws URISyntaxException {
+        // When aopalliance was a transitive API dependency, a modular application
+        // requiring micrometer.tracing and spring.aop would have all three on the module
+        // path.
+        Path tracingPath = getPathForClass(Tracer.class);
+        Path springAopPath = getPathForClass(ProxyFactory.class);
+        Path aopAlliancePath = getAopAlliancePath();
+
+        ModuleFinder finder = ModuleFinder.of(tracingPath, springAopPath, aopAlliancePath);
+
+        Configuration parentConfig = ModuleLayer.boot().configuration();
+
+        // Resolving tracing + spring.aop with aopalliance present fails due to split
+        // package
+        assertThatThrownBy(
+                () -> parentConfig.resolve(finder, ModuleFinder.of(), Set.of("micrometer.tracing", "spring.aop")))
+            .isInstanceOf(ResolutionException.class)
+            .hasMessageContaining("org.aopalliance");
+    }
+
+    private Path getPathForClass(Class<?> clazz) throws URISyntaxException {
+        return Paths.get(clazz.getProtectionDomain().getCodeSource().getLocation().toURI());
+    }
+
+    private Path getAopAlliancePath() {
+        String classPath = System.getProperty("java.class.path");
+        for (String entry : classPath.split(File.pathSeparator)) {
+            if (entry.contains("aopalliance") && !entry.contains("spring")) {
+                return Paths.get(entry);
+            }
+        }
+        throw new IllegalStateException("aopalliance jar not found on classpath");
+    }
+
+}
---
+++ internal/micrometer-tracing-integration-test-spring-aop-with-aspectj/src/test/java/io/micrometer/tracing/integration/spring/withaspectj/SpringAopWithAspectJSpanAspectIntegrationTests.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micrometer.tracing.integration.spring.withaspectj;
+
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.annotation.ContinueSpan;
+import io.micrometer.tracing.annotation.DefaultNewSpanParser;
+import io.micrometer.tracing.annotation.ImperativeMethodInvocationProcessor;
+import io.micrometer.tracing.annotation.MethodInvocationProcessor;
+import io.micrometer.tracing.annotation.NewSpan;
+import io.micrometer.tracing.annotation.SpanAspect;
+import io.micrometer.tracing.annotation.SpanTag;
+import io.micrometer.tracing.test.simple.SimpleSpan;
+import io.micrometer.tracing.test.simple.SimpleTracer;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.Deque;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringAopWithAspectJSpanAspectIntegrationTests {
+
+    SimpleTracer tracer = new SimpleTracer();
+
+    Deque<SimpleSpan> spans = tracer.getSpans();
+
+    @Test
+    void shouldLoadMethodInvocationFromSpringAop() {
+        CodeSource codeSource = MethodInvocation.class.getProtectionDomain().getCodeSource();
+        assertThat(codeSource).isNotNull();
+        URL location = codeSource.getLocation();
+        assertThat(location).isNotNull();
+        assertThat(location.toString()).contains("spring-aop").doesNotContain("aopalliance-1.0");
+    }
+
+    @Test
+    void shouldCreateSpanWithSpringAopProxy() {
+        TestService service = createProxy(new TestServiceImpl());
+        service.annotatedMethod("test-value");
+
+        assertThat(this.spans).hasSize(1);
+        SimpleSpan span = this.spans.peek();
+        assertThat(span.getName()).isEqualTo("custom-span");
+        assertThat(span.getTags()).containsEntry("tagKey", "test-value");
+        assertThat(span.getEndTimestamp().toEpochMilli()).isNotZero();
+        assertThat(this.tracer.currentSpan()).isNull();
+    }
+
+    @Test
+    void shouldContinueSpanWithSpringAopProxy() {
+        TestService service = createProxy(new TestServiceImpl());
+        Span span = this.tracer.nextSpan().name("parent-span").start();
+        try (Tracer.SpanInScope ws = this.tracer.withSpan(span)) {
+            service.continueMethod("continued-value");
+        }
+        finally {
+            span.end();
+        }
+
+        assertThat(this.spans).hasSize(1);
+        SimpleSpan continuedSpan = this.spans.peek();
+        assertThat(continuedSpan.getName()).isEqualTo("parent-span");
+        assertThat(continuedSpan.getTags()).containsEntry("continueTag", "continued-value")
+            .containsEntry("annotated.class", "TestServiceImpl")
+            .containsEntry("annotated.method", "continueMethod");
+        assertThat(continuedSpan.getEvents().stream().map(Map.Entry::getValue)).contains("continueLog.before",
+                "continueLog.after");
+        assertThat(this.tracer.currentSpan()).isNull();
+    }
+
+    private TestService createProxy(TestService target) {
+        MethodInvocationProcessor processor = new ImperativeMethodInvocationProcessor(new DefaultNewSpanParser(),
+                tracer, aClass -> null, aClass -> null);
+        AspectJProxyFactory pf = new AspectJProxyFactory(target);
+        pf.addAspect(new SpanAspect(processor));
+        return pf.getProxy();
+    }
+
+    interface TestService {
+
+        void annotatedMethod(@SpanTag("tagKey") String param);
+
+        void continueMethod(@SpanTag("continueTag") String param);
+
+    }
+
+    static class TestServiceImpl implements TestService {
+
+        @NewSpan(name = "custom-span")
+        @Override
+        public void annotatedMethod(String param) {
+        }
+
+        @ContinueSpan(log = "continueLog")
+        @Override
+        public void continueMethod(String param) {
+        }
+
+    }
+
+}
